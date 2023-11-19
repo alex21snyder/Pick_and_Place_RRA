7814bf109dd594292430ce5e38e643505ab01ab0
alex21snyder <112588065+alex21snyder@users.noreply.github.com>
2023-11-19 15:38:08 -0500

    Delete cross_prod.ino

--- cross_prod.ino
+++
@@ -1,6 +0,0 @@
-
-void cross(const BLA::Matrix<3, 1>& vectorA, const BLA::Matrix<3, 1>& vectorB, BLA::Matrix<3, 1>& result) {
-  result(0, 0) = vectorA(1, 0) * vectorB(2, 0) - vectorA(2, 0) * vectorB(1, 0);
-  result(1, 0) = vectorA(2, 0) * vectorB(0, 0) - vectorA(0, 0) * vectorB(2, 0);
-  result(2, 0) = vectorA(0, 0) * vectorB(1, 0) - vectorA(1, 0) * vectorB(0, 0);
-}
